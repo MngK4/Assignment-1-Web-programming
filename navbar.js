81bcfbe451f329b077053649286dc15ae2626646
Ming <s3938024@rmit.edu.vn>
2023-04-24 04:14:43 +0700

    minor bug fixes

--- navbar.js
+++ navbar.js
@@ -1,8 +1,10 @@
+// Assign variables with ids
 var x = document.getElementById("desktop-header");
 var y = document.getElementById("desktop-main");
 var z = document.getElementById("dropdown-overlay");
-var u = document.getElementsById("dropdown-list")
+var u = document.getElementsById("dropdown-list");
 
+// Add or remove hidden and appear class to elements
 function menuOn() {
     x.classList.add("hidden");
     y.classList.add("hidden");
