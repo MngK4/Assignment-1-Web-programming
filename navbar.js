eeccd09a7cb49a8591b07eed644398545d5bf8fa
Ming <s3938024@rmit.edu.vn>
2023-04-24 01:59:47 +0700

    Finalised web

--- navbar.js
+++ navbar.js
@@ -1,19 +1,18 @@
 var x = document.getElementById("desktop-header");
 var y = document.getElementById("desktop-main");
 var z = document.getElementById("dropdown-overlay");
-
-var img1 = document.getElementById("default-img")
-var img2 = document.getElementById("book-img-2")
-var img3 = document.getElementById("book-img-3")
+var u = document.getElementsById("dropdown-list")
 
 function menuOn() {
     x.classList.add("hidden");
     y.classList.add("hidden");
     z.classList.add("appear");
+    u.classList.add("appear");
 }
 
 function menuOff()  {
     x.classList.remove("hidden");
     y.classList.remove("hidden");
     z.classList.remove("appear");
+    u.classList.remove("appear");
 }
